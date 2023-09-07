7c88140f6d53740cef0d11c08f29b7c4e5a0c26a
Chloe Perou <perouc@gmail.com>
2023-09-07 17:06:07 +0200

    Fichers des profs

---
+++ functionModProjet2023.r
@@ -0,0 +1,93 @@
+### Modele dont la sensibilite doit etre analysee dans le cadre du projet MODE-MPI 2023-2024
+
+### Le modele est ici d?fini sous forme de fonction pour faciliter vos analyses de sensibilite (AS)
+### La fonction renvoie les sorties ponctuelles qui sont a analyser dans l'AS
+
+modAppli <- function(parametre){  
+
+  # CONDITIONS DE SIMULATION
+  temps = 2*365; # nb de pas de temps (en jours)
+  # initialisation pour la sauvegarde de 4 sorties ponctuelles pour chaque jeu de parametres
+  sorties <- matrix(0, nrow=nrow(parametre), ncol=4)
+
+  # boucle des scenarios de l'?chantillonnage de l'AS
+  for (i in 1:nrow(parametre)) { 
+
+    # STRUCTURE & PARAMETRES DU MODELE
+
+    # XX
+    K = parametre[i,1];		# xx
+    sr = parametre[i,2];	# xx
+    m1 = parametre[i,3];	# xx
+    m2 = parametre[i,4];	# xx
+    m3 = parametre[i,5];	# xx
+    f2 = parametre[i,6];	# xx
+    f3 = parametre[i,7];	# xx
+    portee = parametre[i,8];	# xx
+    t1 = parametre[i,9];	# xx
+    t2 = parametre[i,10];	# xx
+
+    # XX
+    trans = parametre[i,11]; # xx
+    lat = parametre[i,12];	# xx
+    rec = parametre[i,13];	# xx
+    loss = parametre[i,14];	# xx
+    madd = parametre[i,15];	# xx
+
+    # INITIALISATION
+    MAT <- array(0, dim=c(4,4,temps)); # nb indiv par classe d'age en ligne (derniere ligne = pop tot), etat de sante en colonne, pas de temps (dimension 3)
+    nvinf <- array(0, dim=c(temps));
+    # conditions initiales (la population est a sa structure d'equilibre, calculee par ailleurs)
+    MAT[1,1,1] <- 27; # xx
+    MAT[2,1,1] <- 23; # xx
+    MAT[3,1,1] <- 36; # xx
+    MAT[3,3,1] <- 1;  # xx
+    # effectifs par etat de sante
+    MAT[4,1,1] <- sum(MAT[1:3,1,1]); MAT[4,2,1] <- sum(MAT[1:3,2,1]); MAT[4,3,1] <- sum(MAT[1:3,3,1]); MAT[4,4,1] <- sum(MAT[1:3,4,1]);
+
+    # SIMULATIONS
+    # boucle du temps
+    for (t in 1:(temps-1)) { 
+     # classe d'age xx
+      # RQ : les naissances sont XX, les nouveaux nes etant dans l'etat XX
+      N <- sum(MAT[4,,t]);	# taille de la pop en t
+	MAT[1,1,t+1] <- MAT[1,1,t]*(1-m1-t1-trans*MAT[4,3,t]/N) + loss*MAT[1,4,t]      + max(0, sr*portee*(sum(MAT[2,,t])*f2 + sum(MAT[3,,t])*f3) * (1 - N/K)); 
+	MAT[1,2,t+1] <- MAT[1,2,t]*(1-m1-t1-lat)			  + trans*MAT[1,1,t]*MAT[4,3,t]/N; 
+	MAT[1,3,t+1] <- MAT[1,3,t]*(1-m1-madd-t1-rec)  		  + lat*MAT[1,2,t]; 
+	MAT[1,4,t+1] <- MAT[1,4,t]*(1-m1-t1-loss) 		  + rec*MAT[1,3,t]; 
+     # classe d'age xx
+	MAT[2,1,t+1] <- MAT[1,1,t]*t1	+ MAT[2,1,t]*(1-m2-t2-trans*MAT[4,3,t]/N) + loss*MAT[2,4,t];
+	MAT[2,2,t+1] <- MAT[1,2,t]*t1	+ MAT[2,2,t]*(1-m2-t2-lat)			+ trans*MAT[2,1,t]*MAT[4,3,t]/N;
+	MAT[2,3,t+1] <- MAT[1,3,t]*t1	+ MAT[2,3,t]*(1-m2-madd-t2-rec)		+ lat*MAT[2,2,t];
+	MAT[2,4,t+1] <- MAT[1,4,t]*t1	+ MAT[2,4,t]*(1-m2-t2-loss)			+ rec*MAT[2,3,t];
+     # classe d'age xx
+	MAT[3,1,t+1] <- MAT[2,1,t]*t2	+ MAT[3,1,t]*(1-m3-trans*MAT[4,3,t]/N) 	+ loss*MAT[3,4,t];
+	MAT[3,2,t+1] <- MAT[2,2,t]*t2	+ MAT[3,2,t]*(1-m3-lat)				+ trans*MAT[3,1,t]*MAT[4,3,t]/N;
+	MAT[3,3,t+1] <- MAT[2,3,t]*t2	+ MAT[3,3,t]*(1-m3-madd-rec)			+ lat*MAT[3,2,t];
+	MAT[3,4,t+1] <- MAT[2,4,t]*t2	+ MAT[3,4,t]*(1-m3-loss)			+ rec*MAT[3,3,t];
+     # calcul des effectifs par etat de sante
+	MAT[4,1,t+1] <- sum(MAT[1:3,1,t+1]); MAT[4,2,t+1] <- sum(MAT[1:3,2,t+1]); MAT[4,3,t+1] <- sum(MAT[1:3,3,t+1]); MAT[4,4,t+1] <- sum(MAT[1:3,4,t+1]);
+	nvinf[t+1]   <- trans*MAT[4,1,t]*MAT[4,3,t]/N
+
+    }# fin boucle temps
+
+    # sorties ponctuelles a analyser
+    # XX
+    sortie1 <- (MAT[4,2,temps]+MAT[4,3,temps])/sum(MAT[4,,temps])
+    # xx
+    sortie2 <- nvinf[temps]
+    # xx
+    sortie3 <- max(MAT[4,3,1:temps])
+    # xx
+    sortie4 <- sum(nvinf[1:365])
+    
+    sorties[i,1] <- sortie1;
+    sorties[i,2] <- sortie2;
+    sorties[i,3] <- sortie3;
+    sorties[i,4] <- sortie4;
+    
+  }# fin boucle scenarios AS
+  return(sorties)
+} # fin fonction du modele
+
+# END
